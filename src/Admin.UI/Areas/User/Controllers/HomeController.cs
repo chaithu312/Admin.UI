6c62e0ac6230f8620451260dcddd7bfa9843a99a
Manoj Kumar <mkumar@ishir.com>
2015-07-20 19:18:23 +0530

    comit
    Signed-off-by: Onur Kutlubay <onur@nybox.com>

---
+++ src/Admin.UI/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Admin.UI.Areas.User.Controllers
+{
+    [Area("User")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Admin.UI/Areas/User/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Admin.UI.Areas.User.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Username required", AllowEmptyStrings = false)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password required", AllowEmptyStrings = false)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Security Question required", AllowEmptyStrings = false)]
+        public string SecurityQuestion { get; set; }
+
+        [Required(ErrorMessage = "Security Answer required", AllowEmptyStrings = false)]
+        public string SecurityAnswer { get; set; }
+    }
+}
