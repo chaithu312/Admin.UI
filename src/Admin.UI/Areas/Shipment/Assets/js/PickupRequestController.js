2934c40a77dae78e30f7492f0a70268eaf44385f
spandit812 <spandit@ishir.com>
2015-09-11 13:54:35 +0530

    OS-119

---
+++ src/Admin.UI/Areas/Shipment/Assets/js/PickupRequestController.js
@@ -0,0 +1,63 @@
+﻿(function () {
+    var app = angular.module('mainApp')
+      app.controller('PickupRequestController', function ($scope, $http) {
+        $scope.Contacts = { Data: [{ Id: 0, Name: 'Select an account...' }, { Id: 1, Name: 'Account 1' }, { Id: 2, Name: 'Account 2' }], selectedOption: { Id: 0, Name: 'Select an account...' } };
+        $scope.Addresses = { Data: [{ Id: 0, Name: 'Select an account...' }, { Id: 1, Name: 'Address 1' }, { Id: 2, Name: 'Address 2' }], selectedOption: { Id: 0, Name: 'Select an account...' } };
+        $scope.States = { Data: [{ Id: 1, Name: 'Address 1' }, { Id: 2, Name: 'Address 2' }] };
+       
+        $scope.Pieces = {
+            Data: [{ Id: 1, Name: '1' },
+                { Id: 2, Name: '2' },
+                { Id: 3, Name: '3' },
+                { Id: 4, Name: '4' },
+                { Id: 5, Name: '5' },
+                { Id: 6, Name: '6' },
+                { Id: 7, Name: '7' },
+                { Id: 8, Name: '8' },
+                { Id: 9, Name: '9' },
+                { Id: 10, Name: '10' },
+                { Id: 11, Name: '11' },
+                { Id: 12, Name: '12' },
+                { Id: 13, Name: '13' },
+                { Id: 14, Name: '14' },
+                { Id: 15, Name: '15' },
+                { Id: 16, Name: '16' },
+                { Id: 17, Name: '17' },
+                { Id: 18, Name: '18' },
+                { Id: 19, Name: '19' },
+                { Id: 20, Name: '20' },
+
+            ], selectedOption: { Id: 1, Name: '1' }
+        };
+
+        $scope.Destination = { Data: [{ Id: 0, Name: 'Domestic' }], selectedOption: { Id: 0, Name: 'Domestic' } };
+        $scope.PickupAgent = { Data: [{ Id: 0, Name: 'Select...' }, { Id: 1, Name: 'DHL' }, { Id: 2, Name: 'Endicia' }, { Id: 3, Name: 'FedEx' }], selectedOption: { Id: 0, Name: 'Select...' } };
+        $scope.PickupType = { Data: [{ Id: 0, Name: 'Package' }, { Id: 1, Name: 'Finance' }], selectedOption: { Id: 0, Name: 'Package' } };
+
+        $scope.sendForm = function () {
+            $http({
+                method: 'POST',
+                url: '/Shipment/PickupRequest',
+                data: $scope.person,
+                headers: {
+                    'RequestVerificationToken': $scope.antiForgeryToken
+                }
+            }).success(function (data, status, headers, config) {
+                $scope.message = '';
+                if (data.success == false) {
+                    var str = '';
+                    for (var error in data.errors) {
+                        str += data.errors[error] + '\n';
+                    }
+                    $scope.message = str;
+                }
+                else {
+                    $scope.message = 'Saved Successfully';
+                    $scope.person = {};
+                }
+            }).error(function (data, status, headers, config) {
+                $scope.message = 'Unexpected Error';
+            });
+        };
+    });
+})();
---
+++ src/Admin.UI/Assets/js/angular-app.js
@@ -0,0 +1,128 @@
+﻿(function () {
+
+    'use strict';
+
+    var app = angular
+        .module("mainApp", ['navsServices'])
+        .config(function ($locationProvider) {
+            $locationProvider.html5Mode(true);
+        });
+
+    app.directive('toggleSidebar', function () {
+        return {
+            restrict: 'E',
+            template: '<!-- #section:basics/sidebar.mobile.toggle --> \
+                       <button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar"> \
+                        <span class="sr-only">Toggle sidebar</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span> \
+                       </button> \
+                       <!-- /section:basics/sidebar.mobile.toggle -->'
+        }
+    })
+
+    app.directive('navbarHeader', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                caption: '@caption'
+            },
+            template: ' <div class="navbar-header pull-left"> \
+                            <!-- #section:basics/navbar.layout.brand --> \
+                            <a href="#" class="navbar-brand"> \
+                                <small> \
+                                    <i class="fa fa-leaf"></i> \
+                                    {{caption}} \
+                                </small> \
+                            </a> \
+                            <!-- /section:basics/navbar.layout.brand --> \
+                        </div>'
+        }
+    })
+
+    app.directive('sidebarMinimize', function () {
+        return {
+            restrict: 'E',
+            template: ' <!-- #section:basics/sidebar.layout.minimize --> \
+                        <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse"> \
+                            <i class="ace-icon fa fa-angle-double-left" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i> \
+                        </div> \
+                        <!-- /section:basics/sidebar.layout.minimize -->'
+        }
+    })
+
+    //the module we are demonstrating:
+    app.directive('autoActive', ['$location', '$timeout', function ($location, $timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element) {
+                function setActive() {
+                    var path = $location.path();
+                    if (path) {
+                        angular.forEach(element.find('li'), function (li) {
+                            console.log(li)
+                            var anchor = li.querySelector('a');
+                            if (anchor.href.match(path + '(?=\\?|$)')) {
+                                angular.element(li).addClass('active');
+                            } else {
+                                angular.element(li).removeClass('active');
+                            }
+                        });
+                    }
+                }
+                $timeout(setActive());
+                scope.$on('$locationChangeSuccess', setActive);
+            }
+        }
+    }]);
+})();
+
+//-- Navigation Controller
+(function () {
+    
+    'use strict';
+    angular.module('mainApp').controller('navsController', navsController)
+    navsController.$inject = ['$scope', 'Navs'];
+    function navsController($scope, Navs) {
+        $scope.init = function () {
+            $scope.navs = Navs.query();
+        }
+    }
+
+    //window.location.hash = '#/'; //All hash paths need to start with a /, it happens automaticaly with ngResource and the like...
+
+
+    // in controller
+ 
+})();
+
+//-- Navigation Services
+(function () {
+    'use strict';
+    var navsServices = angular.module('navsServices', ['ngResource']);
+    navsServices.factory('Navs', ['$resource', function ($resource) {
+        return $resource('/api/Navigation/', {}, {
+            query: { method: 'GET', params: {}, isArray: true }
+        });
+    }]);
+})();
+
+//--UserController for registration
+
+//var app = angular.module("myApp", []);
+//app.controller("userCntrl", function ($scope, $http) {
+//    alert("hi");
+//    if (!Valid) {
+//        alert("Invalid form");
+//        return;
+//    } else {
+//        alert("It's Great. Form Submitted");
+//    }
+
+//    $scope.Save = function (Valid) {
+//        if (!Valid) {
+//            alert("Invalid form");
+//            return;
+//        } else {
+//            alert("It's Great. Form Submitted");
+//        }
+//    }
+//});
