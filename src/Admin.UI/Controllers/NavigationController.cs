36956edd944161202e9e5d087bbd5ccbf0cba319
mkumarishir <mkumar@ishir.com>
2015-10-13 13:56:07 +0530

    OS-169

--- src/Admin.UI/Controllers/NavigationController.cs
+++ src/Admin.UI/Controllers/NavigationController.cs
@@ -15,22 +15,25 @@ namespace Admin.UI.Controllers
             {
                 //Parent Navigation
 
-                new Navigation { Id = 1, Title = "Dashboard", ParentId = 0, Type = "", NavURL = "/AdminUI/Index" },
-                new Navigation { Id = 2, Title = "Shipments ", ParentId = 0, Type = "", NavURL = "/AdminUI/Shipment" },
-                new Navigation { Id = 3, Title = "User", ParentId = 0, Type = "", NavURL = "/AdminUI/User" } ,
-                new Navigation { Id = 4, Title = "Finance", ParentId = 0, Type = "", NavURL = "/AdminUI/Finance" },
+                new Navigation { Id = 1, Title = "Dashboard", ParentId = 0, Type = "", NavURL = "/Index" },
+                new Navigation { Id = 2, Title = "Shipments ", ParentId = 0, Type = "", NavURL = "/Shipment" },
+                new Navigation { Id = 3, Title = "User", ParentId = 0, Type = "", NavURL = "/User" } ,
+                new Navigation { Id = 4, Title = "Finance", ParentId = 0, Type = "", NavURL = "/Finance" },
 
                 //Child Navigation
-                new Navigation { Id = 5, Title = "AWB with Price", ParentId = 2, Type = "", NavURL = "/AdminUI/Shipment/AWBwithPrice" },
-                new Navigation { Id = 6, Title = "Pickup Request ", ParentId = 2, Type = "", NavURL = "/AdminUI/Shipment/PickUpRequest" },
-                new Navigation { Id = 7, Title = "View Pickup", ParentId = 2, Type = "", NavURL = "/AdminUI/Shipment/ViewPickup" },
-                new Navigation { Id = 8, Title = "Tracking", ParentId = 2, Type = "", NavURL = "/AdminUI/Shipment/Tracking" } ,
-                new Navigation { Id = 8, Title = "Vendor Setting", ParentId = 2, Type = "", NavURL = "/AdminUI/Shipment/VendorSetting" } ,
 
-                new Navigation { Id = 9, Title = "Create User", ParentId = 3, Type = "", NavURL = "/AdminUI/User" },
-                new Navigation { Id = 10, Title = "View User ", ParentId = 3, Type = "", NavURL = "/AdminUI/User/List" },
-                new Navigation { Id = 11, Title = "AddressBook", ParentId = 3, Type = "", NavURL = "/AdminUI/User/AddressBook" },
-                new Navigation { Id = 12, Title = "View Address", ParentId = 3, Type = "", NavURL = "/AdminUI/User/ViewAddress" }
+                new Navigation { Id = 5, Title = "AWB with Price", ParentId = 2, Type = "", NavURL = "/Shipment/AWBwithPrice" },
+                new Navigation { Id = 6, Title = "Pickup Request ", ParentId = 2, Type = "", NavURL = "/Shipment/PickUpRequest" },
+                new Navigation { Id = 7, Title = "View Pickup", ParentId = 2, Type = "", NavURL = "/Shipment/ViewPickup" },
+                new Navigation { Id = 8, Title = "Tracking", ParentId = 2, Type = "", NavURL = "/Shipment/Tracking" } ,
+                new Navigation { Id = 8, Title = "Vendor Setting", ParentId = 2, Type = "", NavURL = "/Shipment/VendorSetting" } ,
+
+                new Navigation { Id = 9, Title = "Create User", ParentId = 3, Type = "", NavURL = "/User" },
+                new Navigation { Id = 10, Title = "View User ", ParentId = 3, Type = "", NavURL = "/User/List" },
+                new Navigation { Id = 11, Title = "AddressBook", ParentId = 3, Type = "", NavURL = "/User/AddressBook" },
+                new Navigation { Id = 12, Title = "View Address", ParentId = 3, Type = "", NavURL = "/User/ViewAddress" },
+
+                new Navigation { Id = 13, Title = "Home", ParentId = 1, Type = "", NavURL = "/default" },
             };
         }
     }
