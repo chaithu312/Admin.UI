809b939f4d1ab3342e7060da90358c1829cab0f4
spandit812 <spandit@ishir.com>
2016-01-22 20:41:41 +0530

    Freight-OS-287

--- src/Admin.UI/Gruntfile.js
+++ src/Admin.UI/Gruntfile.js
@@ -82,6 +82,12 @@
                         'Areas/Finance/Assets/js/PaymentController.js',
                         'Areas/Freight/Assets/js/FreightRequestsController.js',
                         'Areas/Freight/Assets/js/ViewFreightRequestsController.js',
+                        'Areas/Freight/Assets/js/FreightCostItemController.js',
+                        'Areas/Freight/Assets/js/ViewFreightCostItemController.js',
+                        'Areas/Freight/Assets/js/FreightMessageController.js',
+                        'Areas/Freight/Assets/js/ViewMessageController.js',
+                        'Areas/Freight/Assets/js/FreightSignatureController.js',
+                        'Areas/Freight/Assets/js/ViewSignatureController.js',
                     ],
                     'wwwroot/assets/js/DataTable.js': [
                         'Assets/ace/js/dataTables/jquery.dataTables.js',
