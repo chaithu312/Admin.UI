25fdcaf84df8afd3ba603d5af7e60e036f0870b9
Chaitanya <kchaitanyareddy@gmail.com>
2016-05-02 15:57:08 -0400

    Commit after merge

--- src/Admin.UI/Areas/Freight/Models/CostItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Admin.UI.Areas.Freight.Models
-{
-	public class CostItem
-    {
-		public long Id { get; set; }
-		public string ServiceName { get; set; }
-		public string Created { get; set; }
-	}
-}
